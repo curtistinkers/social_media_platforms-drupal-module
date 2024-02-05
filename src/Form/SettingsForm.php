8eaea5d12bab521aad3cf0dfdfc6ff34638898c3
Guiu Rocafort Ferrer <guiu.rocafort@ext.simonelectric.com>
2024-02-05 15:34:34 +0100

    Initial code commit

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\social_media_platforms\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Social Media Platforms settings for this site.
+ */
+final class SettingsForm extends ConfigFormBase {
+
+  const SOCIAL_PLATFORMS = [
+    'facebook' => 'Facebook',
+    'youtube' => 'Youtube',
+    'linkedin' => 'Linkedin',
+    'x' => 'X',
+    'instagram' => 'Instagram',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'social_media_platforms_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['social_media_platforms.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $config = $this->config('social_media_platforms.settings');
+
+    foreach (self::SOCIAL_PLATFORMS as $key => $social_platform) {
+      $form[$key . '_url'] = [
+        '#type' => 'url',
+        '#title' => $social_platform,
+        '#default_value' => $config->get($key),
+      ];
+    }
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+
+    $config = $this->configFactory()->getEditable('social_media_platforms.settings');
+
+    foreach (self::SOCIAL_PLATFORMS as $key => $social_platform) {
+      $value = $form_state->getValue($key . '_url');
+      $config->set($key, $value);
+    }
+
+    $config->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\social_media_platforms\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\Core\Theme\ThemeManager;
+use Drupal\Core\Extension\ExtensionPathResolver;
+use Drupal\Core\Template\Attribute;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+/**
+ * Provides a Social Media Links block.
+ *
+ * @Block(
+ *   id = "social_media_platform_block",
+ *   admin_label = @Translation("Social Media Platform Links"),
+ *   category = @Translation("Social Media Platforms"),
+ * )
+ */
+class SocialMediaBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+    /**
+   * Constructs a Drupalist object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Theme\ThemeManager $themeManager
+   *   The theme manager.
+   * @param \Drupal\Core\Extension\ExtensionPathResolver $pathResolver
+   *   The path resolver.
+   */
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    protected ThemeManager $themeManager,
+    protected ExtensionPathResolver $pathResolver,
+  ) {
+    parent::__construct($configuration,
+    $plugin_id,
+    $plugin_definition,
+  );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('theme.manager'),
+      $container->get('extension.path.resolver'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config =  \Drupal::config('social_media_platforms.settings')->get();
+    $output = [];
+    $links = [];
+    $path = $this->pathResolver->getPath('module', 'social_media_platforms');
+
+    foreach ($config as $key => $value) {
+      $imagePath = '/' . $path . '/images/' . $key.'.png';
+      $links[$key] = [
+        'link_url' => $value,
+        'image_url' => $imagePath,
+        'attributes' => new Attribute(),
+        'title' =>  $key,
+      ];
+    }
+
+    $this->themeManager->alter('social_media_platforms', $links);
+
+    $output['links'] = [
+      '#attributes' => new Attribute(),
+      '#theme' => 'social_media_platforms_links',
+      '#links' => $links,
+    ];
+
+    return $output;
+  }
+
+}
